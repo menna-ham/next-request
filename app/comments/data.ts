1a8c475b0cb0cb56318aa0bb66ba4a64d43fc8d3
menna-ham <menna.hamouda58@gmail.com>
2024-03-02 14:27:10 +0200

    get&post

---
+++ app/comments/data.ts
@@ -0,0 +1,18 @@
+export const comments=[
+    {
+        id:1,
+        text:'Comment Text Number 1'
+    },
+    {
+        id:2,
+        text:'Comment Text Number 2'
+    },
+    {
+        id:3,
+        text:'Comment Text Number 3'
+    },
+    {
+        id:4,
+        text:'Comment Text Number 4'
+    }
+]

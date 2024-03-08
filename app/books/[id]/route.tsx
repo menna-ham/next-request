6c4e8b4e26917cb5dab9238945d2b944d105a2c4
menna-ham <menna.hamouda58@gmail.com>
2024-03-08 14:47:52 +0200

    updating

--- app/books/[id]/route.tsx
+++ app/books/[id]/route.tsx
@@ -1,8 +1,8 @@
 import { books } from "../data";
-import { NextURL } from "next/dist/server/web/next-url";
 import { redirect } from "next/navigation";
+import { NextRequest, NextResponse } from "next/server";
 
-export async function GET(request:NextURL,{params}:{params:{id:string}}){
+export async function GET(request:NextRequest,{params}:{params:{id:string}}){
     let foundBook= books.find(b=>b.id===parseInt(params.id))
     
     if(parseInt(params.id)>books.length )
@@ -15,12 +15,31 @@ export async function GET(request:NextURL,{params}:{params:{id:string}}){
     }
 }
 
-export async function DELETE (request:NextURL, {params}:{params:{id:string}}){
+export async function DELETE (request:NextRequest, {params}:{params:{id:string}}){
     let index = books.findIndex(b=>b.id ===parseInt(params.id))
     let delBooks = books[index]
     let filtered = books.splice(index,1);
     return Response.json(books)
-    // let deleIndex = books.findIndex((b)=>b.id===parseInt(params.id))
-    // let delBook = books[deleIndex];
-    // let filtered books.splice()
+}
+
+
+export async function PATCH(request:Request, {params}:{params:{id:string}}){
+    let req = await request.json();
+    let {title,pageCount} = req;
+    let index =books.findIndex(b=>b.id === parseInt(params.id))
+    if(index){
+        books[index].title=title;
+        books[index].pageCount = pageCount;
+    }
+    
+    return NextResponse.json(books[index])
+    // else if (index === null || index === undefined || index >= books.length){
+    //     return Response.json(`Couldn't find comment with ID:${params.id}  please enter valid ID`, {
+    //         status:404,
+    //         headers:{
+    //             'Content-Type':'application/json'
+    //         }})
+    // }
+
+
 }

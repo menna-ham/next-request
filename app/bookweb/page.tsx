deb8a51c75539ee990af702b549a2556f6038343
menna-ham <menna.hamouda58@gmail.com>
2024-03-16 16:22:47 +0200

    trying to fetch using localhost

--- app/bookweb/page.tsx
+++ app/bookweb/page.tsx
@@ -1,18 +1,30 @@
-import React from 'react'
+"use client"
+import React, { useRef } from 'react'
+import { books } from '../books/data';
 
 type Props = {}
 
 const BookWeb = (props: Props) => {
+  let serRef = useRef<HTMLInputElement>(null);
+
+  let SerButton =async (e:React.MouseEvent<HTMLButtonElement>)=>{
+    e.preventDefault();
+    let SerVal = serRef.current?.value;
+    // let filtered = books.filter(b=>b.title.includes(SerVal))
+  }
     
   return (
-    <div className='bg-green-100 p-4 h-[100vh]'>
-        <p className='text-2xl'> Search for your desired book</p>
+    <div className='bg-green-100 p-4 h-[100vh] m-auto'>
+        <p className='text-2xl text-center'> Search for your desired book</p>
     
-        <form className='flex flex-row'>
-        <div className='my-3 w-[25%]'>
-            <input className='w-full p-2 rounded border-[1px] border-gray-100 ' placeholder='Search '/>
-        </div>
-        <button type='submit' className='bg-gray-500 text-white  px-2 rounded mx-3'>Search</button>
+        <form className='flex flex-row justify-center my-5'>
+
+          <div className='my-3 w-[40%]'>
+              <input ref={serRef} className='w-full p-2 rounded border-[1px] border-gray-100 ' placeholder='Search '/>
+          </div>
+
+          <button onClick={(e:React.MouseEvent<HTMLButtonElement>)=>SerButton(e)} type='submit' className='bg-gray-500 text-white  px-2 rounded mx-3'>Search</button>
+
         </form>
 
 
